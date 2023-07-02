002baddbe15f4e68fb637cdc25876d5d4cffb5a5
Dagdyr <qwertydanielka2009@gmail.com>
2023-07-02 19:17:49 +0300

    posts fix btn

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -63,7 +63,7 @@
                             <div class="modal-content">
                                 <div class="modal-header">
                                     <h5 class="modal-title" id="modalLabelCreateFeed">Добавить запись</h5>
-                                    <button type="button" class="btn-close" data-bs-dismiss="Добавить запись" aria-label="Close"></button>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                 </div>
                                 <div class="modal-body">
                                     <div class="d-flex mb-3">
